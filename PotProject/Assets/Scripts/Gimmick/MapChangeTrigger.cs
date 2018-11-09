ab10a042775dc68b16f01a09ed56f9ec9ef55419
mamanju <himazin19970630@gmail.com>
2018-11-09 13:15:32 +0900

    とりあえずコミット

---
+++ PotProject/Assets/Scripts/Gimmick/MapChangeTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChangeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("kitao");
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.SetParent(transform.root.gameObject.transform);
+        }
+    }
+}
