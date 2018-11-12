0011cd52a29c4a920204dfd3ff23aa219c82f2ec
mamanju <himazin19970630@gmail.com>
2018-11-12 17:18:31 +0900

    少し修正

--- PotProject/Assets/Scripts/Map/MapInfo.cs
+++ PotProject/Assets/Scripts/Map/MapInfo.cs
@@ -9,7 +9,6 @@ public class MapInfo : MonoBehaviour {
     public GameObject rock;
     [SerializeField]
     private bool _wood; // 伸びる木
-    [SerializeField]
     private bool _upRock; // スイッチ岩
     [SerializeField]
     private int ladderCount;
@@ -17,10 +16,26 @@ public class MapInfo : MonoBehaviour {
     private bool[] _upLadders; // 縄ハシゴ
     private bool[] _downLadders; // 縄ハシゴ
 
+    public bool UpRock
+    {
+        get {return this._upRock;}
+        set {value = this._upRock;}
+    }
+
+    public bool[] UpLaddersFlag
+    {
+        get { return this._upLadders; }
+        set { value = this._upLadders; }
+    }
+
+    public bool[] DownLaddersFlag
+    {
+        get { return this._downLadders; }
+        set { value = this._downLadders; }
+    }
+
     void Awake() {
         this._upLadders = new bool[this.ladderCount];
         this._downLadders = new bool[this.ladderCount];
-
-        Debug.Log(_upLadders.Length);
     }
 }
