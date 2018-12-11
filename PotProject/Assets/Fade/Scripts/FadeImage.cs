6d784aa89369bc5ef35491c84e40662959eca477
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2018-12-11 19:58:24 +0900

    カメラの切り替え処理追加

--- PotProject/Assets/Fade/Scripts/FadeImage.cs
+++ PotProject/Assets/Fade/Scripts/FadeImage.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class FadeImage : UnityEngine.UI.Graphic , IFade
+public class FadeImage : UnityEngine.UI.Graphic, IFade
 {
 	[SerializeField]
 	private Texture maskTexture = null;
@@ -28,44 +28,13 @@ public class FadeImage : UnityEngine.UI.Graphic , IFade
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(test(1));
-        }
+
     }
 
-    IEnumerator test(float interval)
-    {
-        float time = 0;
-        while (time <= interval)
-        {
-            cutoutRange = Mathf.Lerp(0f, 1f, time / interval);
-            time += Time.unscaledDeltaTime;
-            UpdateMaskCutout(cutoutRange);
-            yield return 0;
-        }
-
-        yield return new WaitForSeconds(1.0f);
-
-        //だんだん明るく .
-        time = 0;
-        while (time <= interval)
-        {
-            cutoutRange = Mathf.Lerp(1f, 0f, time / interval);
-            time += Time.unscaledDeltaTime;
-            UpdateMaskCutout(cutoutRange);
-            yield return 0;
-        }
-    }
-
-    private void UpdateMaskCutout (float range)
+    public void UpdateMaskCutout (float range)
 	{
 		enabled = true;
 		material.SetFloat ("_Range", 1 - range);
-
-		//if (range <= 0) {
-		//	this.enabled = false;
-		//}
 	}
 
 	public void UpdateMaskTexture (Texture texture)
--- PotProject/Assets/Scripts/CameraManager.cs
+++ PotProject/Assets/Scripts/CameraManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
 
+    private FadeImage fade;
+
     private Camera mainCamera;
     private Camera subCamera;
 
     void Awake()
     {
         mainCamera = Camera.main;
-        subCamera = Camera.allCameras[1];
+        subCamera = Camera.allCameras[0];
+        fade = FindObjectOfType<FadeImage>();
     }
 
     void Start () {
@@ -18,6 +21,90 @@ public class CameraManager : MonoBehaviour {
 	}
 	
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            FadeInOutCamera(new Vector2(16, 0));
+        }
 	}
+
+    private void FadeInOutCamera(Vector2 cameraPos)
+    {
+        StartCoroutine(FadeInOut(cameraPos, 2));
+    }
+
+    private void FadeInOutCamera(Vector3 cameraPos, float waitTime)
+    {
+        StartCoroutine(FadeInOut(cameraPos, waitTime));
+    }
+
+    IEnumerator FadeInOut(Vector2 cameraPos, float waitTime)
+    {
+        //  黒いテクスチャーで画面を隠す
+        float time = 0;
+        float interval = 0.5f;
+        while (time <= interval)
+        {
+            fade.Range = Mathf.Lerp(0f, 1f, time / interval);
+            time += Time.unscaledDeltaTime;
+            yield return 0;
+        }
+        fade.Range = 1;
+
+        //  カメラの変更処理
+        SwitchingCameraSub(cameraPos);
+        yield return new WaitForSeconds(0.5f);
+
+        //  黒のテクスチャを取りのぞく
+        time = 0;
+        while (time <= interval)
+        {
+            fade.Range = Mathf.Lerp(1f, 0f, time / interval);
+            time += Time.unscaledDeltaTime;
+            yield return 0;
+        }
+        fade.Range = 0;
+        yield return null;
+
+        //  サブカメラで見ている時間
+        yield return new WaitForSeconds(waitTime);
+
+        //  黒いテクスチャーで画面を隠す
+        time = 0;
+        while (time <= interval)
+        {
+            fade.Range = Mathf.Lerp(0f, 1f, time / interval);
+            time += Time.unscaledDeltaTime;
+            yield return 0;
+        }
+        fade.Range = 1;
+
+        //  カメラの変更処理
+        SwitchingCameraMain();
+        yield return new WaitForSeconds(0.5f);
+
+        //  黒のテクスチャを取りのぞく
+        time = 0;
+        while (time <= interval)
+        {
+            fade.Range = Mathf.Lerp(1f, 0f, time / interval);
+            time += Time.unscaledDeltaTime;
+            yield return 0;
+        }
+        fade.Range = 0;
+        yield return null;
+    }
+
+    //  メインカメラのアクティブ化
+    private void SwitchingCameraMain()
+    {
+        mainCamera.gameObject.SetActive(true);
+        subCamera.gameObject.SetActive(false);
+    }
+    //  サブカメラのアクティブ化
+    private void SwitchingCameraSub(Vector2 cameraPos)
+    {
+        subCamera.transform.position = new Vector3(cameraPos.x, cameraPos.y, -10);
+        subCamera.gameObject.SetActive(true);
+        mainCamera.gameObject.SetActive(false);
+    }
 }
