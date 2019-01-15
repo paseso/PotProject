92f5c1d5c4f6a6325e4b490577071cc5ec0d4c1d
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-01-15 18:33:12 +0900

    バルーンの拡大縮小処理の追加

--- PotProject/Assets/Scripts/TextBalloon.cs
+++ PotProject/Assets/Scripts/TextBalloon.cs
@@ -3,28 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class TextBalloon : MonoBehaviour {
 
+    private PlayerController playerController;
+
     [SerializeField]
     private string[] massages;
-    private const string massage = "あああ";
     [SerializeField]
     private GameObject balloonPrefab;
+    [SerializeField,Range(0, 0.3f)]
+    float intervalForCharDisplay = 0.2f;           // 1文字の表示にかける時間
 
     private GameObject canvas;
     private GameObject massageUI;
-
-    [SerializeField,Range(0, 0.3f)]
-    float intervalForCharDisplay = 0.2f;           // 1文字の表示にかける時間
 
     private bool isTalk = false;
     private int currentSentenceNum = 0;             //現在表示している文章番号
     private string currentSentence = string.Empty;  // 現在の文字列
     private float timeBeginTalk = 0;                // 会話が始まってからの時間
-    private float timeUntilDisplay = 0;             // 表示にかかる時間
-    private float timeBeganDisplay = 1;             // 文字列の表示を開始した時間
-    private int lastUpdateCharCount = -1;           // 表示中の文字数
 
     // イベント開始のフラグがたったら会話が始まる（その間はキャラの操作は無効）
     // 1文章を1文字ずつ表示する
@@ -34,51 +32,65 @@ public class TextBalloon : MonoBehaviour {
 
     void Start () {
         canvas = GameObject.Find("Canvas");
-        massageUI = Instantiate(balloonPrefab) as GameObject;
-        massageUI.transform.SetParent(canvas.transform, false);
-	}
+        playerController = FindObjectOfType<PlayerController>();
+        StartCoroutine(InstanceBalloon());
+    }
 	
 	void Update () {
-
-        if (isTalk) timeBeginTalk += Time.deltaTime;
+        // デバッグ用
         if (Input.GetKey(KeyCode.Space)) isTalk = !isTalk;
-        if (Input.GetKeyDown(KeyCode.Return)) TapEnter();
-
-        Debug.Log("Cast なし" + timeBeginTalk / intervalForCharDisplay);
-
-        // 文章が最後まで表示していなかったら
-        if (massages[currentSentenceNum].Length >= (timeBeginTalk / intervalForCharDisplay))
+        
+        // 会話が開始したら
+        if (isTalk)
         {
-            Debug.Log("bbb");
-            Debug.Log("Cast あり" +(int)(timeBeginTalk / intervalForCharDisplay));
-            Debug.Log("Cast なし" + timeBeginTalk / intervalForCharDisplay);
-            currentSentence = massages[currentSentenceNum].Substring(0, (int)(timeBeginTalk / intervalForCharDisplay));
-        }        
-
-        if (massages[currentSentenceNum].Length > currentSentence.Length)
-        {
-            Debug.Log("aaa");
-            DisplayText(currentSentence);
+            // 文章が最後まで表示していなかったら
+            if (massages[currentSentenceNum].Length >= timeBeginTalk / intervalForCharDisplay)
+            {
+                timeBeginTalk += Time.deltaTime;
+                currentSentence = massages[currentSentenceNum].Substring(0, (int)(timeBeginTalk / intervalForCharDisplay));
+                massageUI.transform.GetComponentInChildren<Text>().text = currentSentence;
+            }
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetButton("Circle"))
+            {
+                NextWard();
+            }
         }
     }
 
-    private void TapEnter()
+    private void NextWard()
     {
-        if (true)
+        if (massages.Length > currentSentenceNum + 1)
         {
             currentSentenceNum++;
             timeBeginTalk = 0;
         }
+        else
+        {
+            StartCoroutine(DisableBalloon());
+        }
     }
 
-    //void StartTalk()
-    //{
-    //    string showText = "";
-    //    DisplayText(massages[currentSentenceNum]);
-    //}
-
-    void DisplayText(string text)
+    // 吹き出しの生成・会話の開始
+    IEnumerator InstanceBalloon()
     {
-        massageUI.transform.GetChild(0).GetComponent<Text>().text = text;
+        yield return new WaitForSeconds(0.1f);
+        massageUI = Instantiate(balloonPrefab) as GameObject;
+        massageUI.transform.SetParent(canvas.transform, false);
+        massageUI.transform.localScale = new Vector3(0.75f, 0.75f, 1);
+        massageUI.transform.DOScale(new Vector2(1, 1), 0.1f);
+        yield return new WaitForSeconds(0.15f);
+        isTalk = true;
+        yield break;
+    }
+    // 会話の終了
+    IEnumerator DisableBalloon()
+    {
+        isTalk = false;
+        yield return null;
+        massageUI.transform.DOScale(new Vector2(0.75f, 0.75f), 0.1f);
+        massageUI.GetComponentInChildren<Text>().text = "";
+        yield return new WaitForSeconds(0.1f);
+        massageUI.SetActive(false);
+        yield break;
     }
 }
