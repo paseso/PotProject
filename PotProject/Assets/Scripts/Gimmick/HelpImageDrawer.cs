6871bc4b6e61757a468c880b512e106eca81358b
mametaro117 <ytfeek1@yahoo.co.jp>
2019-02-01 01:07:28 +0900

    ヘルプ用の画像をパラパラ漫画風に表示する

--- PotProject/Assets/Scripts/Gimmick/HelpImageDrawer.cs
+++ PotProject/Assets/Scripts/Gimmick/HelpImageDrawer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,16 +8,82 @@ public class HelpImageDrawer : MonoBehaviour {
     [SerializeField]
     private Sprite[] helpSprites;
 
-    [SerializeField, Range(1, 10)]
-    private float nextSpriteTime;
+    private const float  nextSpriteTime = 3;    //  画像が変わる時間
+    private bool onReady = false;               //  Spriteが存在するか
+    private float rideTime = 0f;                //  乗られている時間
 
-	// Use this for initialization
-	void Start () {
-		
+    private GimmickController gimmick;
+    private SpriteRenderer spr;
+
+
+	void Start ()
+    {
+        Init();
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    private void Init()
+    {
+        if (helpSprites.Length <= 0)
+        {
+            onReady = false;
+            return;
+        }
+        gimmick = gameObject.GetComponentInChildren<GimmickController>();
+        GameObject helpObject = new GameObject("Helpimage");
+        helpObject.transform.parent = gameObject.transform;
+        helpObject.transform.localPosition = new Vector3(0, 6, 0);
+        helpObject.AddComponent<SpriteRenderer>();
+        spr = helpObject.GetComponent<SpriteRenderer>();
+        spr.enabled = false;
+        spr.sprite = helpSprites[0];
+        onReady = true;
+    }
+
+
+    void Update () {
+        if (onReady)
+            ImageDrawer();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    /// <summary>
+    /// スプライトを乗っている間はパラパラで表示する
+    /// </summary>
+    private void ImageDrawer()
+    {
+        //  プレイヤーが乗っていたら
+        if (gimmick.OnPlayerFlag)
+        {
+            if (rideTime < 1)
+            {
+                spr.enabled = true;
+                rideTime += 2 * Time.deltaTime;
+                rideTime = Mathf.Min(rideTime, 1);
+                spr.color = new Color(spr.color.r, spr.color.g, spr.color.b, rideTime);
+            }
+            else
+            {
+                rideTime += Time.deltaTime;
+                int spriteNum = 0;
+                spriteNum = (int)(rideTime / nextSpriteTime) % helpSprites.Length;
+                spr.sprite = helpSprites[spriteNum];
+            }
+        }
+        //  プレイヤーが下りたら
+        else
+        {
+            if (rideTime <= 0) return;
+            if (rideTime >= 1) rideTime = 1;
+            rideTime -= Time.deltaTime;
+            rideTime = Mathf.Max(rideTime, 0);
+            if (rideTime <= 0)
+            {
+                spr.enabled = false;
+                return;
+            }
+            spr.color = new Color(spr.color.r, spr.color.g, spr.color.b, rideTime);
+        }
+    }
 }
